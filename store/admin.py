b198a02a54f7feb08efe6b822cdd8cdcea80bd40
Alireza Amouzadeh <alireza@amouzadeh.net>
2021-06-04 16:11:04 +0430

    WIP Inventory & shopping.

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    """
+    Admin inline for OrderItem model
+    """
+    model = models.OrderItem
+    fields = (
+        'product',
+        'qty',
+        'price',
+    )
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (
+        OrderItemInline,
+    )
