7f78a7b829a50b6e8467288cc2c6667a74318ce7
Alireza Amouzadeh <alireza@amouzadeh.net>
2021-06-04 15:42:25 +0430

    WIP Inventory & shopping.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.site_header = 'فروشگاه هنری من'
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'description',
+        'price',
+        'qty_in_stock',
+        'is_active',
+        'type',
+    )
+    list_display_links = (
+        'pk',
+        'name',
+    )
+    list_editable = (
+        'price',
+        'qty_in_stock',
+    )
+    search_fields = (
+        'name',
+        'description__icontains',
+    )
+    list_filter = (
+        'is_active',
+        'type',
+    )
