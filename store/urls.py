ce4a3570f68ba344742233256ce415125c658706
Alireza Amouzadeh <alireza@amouzadeh.net>
2021-06-11 16:50:28 +0430

    WIP REST API for orders.

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
+# Use a default router to generate urls for ViewSets
 router = DefaultRouter()
 router.register('orders', views.OrderViewSet)
 
