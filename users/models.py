7fc7405068683353d2170619149b0a16af9fc3c2
Alireza Amouzadeh <alireza@amouzadeh.net>
2021-06-24 15:59:52 +0430

    Added signals.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=12, blank=True, null=True)
--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,9 @@
-# from django.db.models.signals import post_save
-from store.signals import order_placed
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+from store.signals import order_placed
+from users.models import Profile
 
 
 @receiver(order_placed)
@@ -9,3 +12,12 @@ def send_email_when_order_is_placed(sender, **kwargs):
     A call back for sending email when order is placed.
     """
     print(f"Hello from signals. {kwargs['created']}")
+
+
+@receiver(post_save, sender=get_user_model())
+def create_profile_for_new_users(sender, **kwargs):
+    """
+    Creates profile for new users
+    """
+    if kwargs['created']:
+        Profile.objects.create(user=kwargs['instance'])
