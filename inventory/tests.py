470217f470c330313dd8964884f1653ddad0f722
Alireza Amouzadeh <alireza@amouzadeh.net>
2021-06-25 11:53:39 +0430

    Added tests.

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Product
+
+
+class ProductTestCase(TestCase):
+    """
+    Tests for Product model
+    """
+
+    def setUp(self):
+        """
+        Set up the test requirements
+        """
+        from .enums import ProductTypes
+        Product.objects.create(
+            name='Test PROD1',
+            description='Test PROD1 Desc',
+            qty_in_stock=100,
+            is_active=True,
+            type=ProductTypes.PRINT
+        )
+
+    def test_deduct_from_stock(self):
+        """
+        Tests deduct_from_stock model method
+        """
+        obj = Product.objects.first()
+        qty = obj.deduct_from_stock(10)
+        self.assertEqual(qty, 90)
