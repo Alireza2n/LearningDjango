f26c306348f56a62ebd2f13ee53b44cea7219a92
Alireza Amouzadeh <alireza@amouzadeh.net>
2021-06-29 20:53:37 +0430

    Added tests.

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
@@ -20,3 +21,13 @@ class MyOrdersViewTestCase(TestCase):
 
         response = self.client.get(reverse('store:list-orders'), follow=True)
         self.assertContains(response, 'Please login')
+
+    def test_empty_order_history_for_user(self):
+        """
+        A new user should see an empty history
+        """
+        user = get_user_model().objects.create(username='user1')
+        self.client.force_login(user=user)
+        response = self.client.get(reverse('store:list-orders'))
+        self.assertContains(response, 'تاریخ سفارش')
+        self.assertEqual(response.context_data['paginator'].count, 0)
